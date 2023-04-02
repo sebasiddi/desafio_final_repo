677d99fbdb833c09cb69b54308e7517bd73e9cd0
Sebastian Siddi <ssiddi@gmail.com>
2023-04-02 17:50:51 -0300

    version 2023 para Heroku

--- desafio_final/desafio_final/settings.py
+++ desafio_final/desafio_final/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url  
 from django.core.wsgi import get_wsgi_application  
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,11 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'portal/static'),)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miSitio.settings")  
 application = get_wsgi_application()  
  
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
